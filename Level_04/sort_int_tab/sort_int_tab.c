6d4aa014476dd82f74c5239e5eb1e8fd075044ff
yassine benmansour <99927722+yassinbenmansour@users.noreply.github.com>
2022-07-14 23:41:38 +0100

    update

---
+++ Level_04/sort_int_tab/sort_int_tab.c
@@ -0,0 +1,21 @@
+void	sort_int_tab(int *tab, unsigned int size)
+{
+	unsigned int i;
+	int tmp;
+
+	i = 0;
+	if (tab != NULL)
+	{
+		while (i < size)
+		{
+			if (tab[i] > tab[i + 1])
+			{
+				tmp = tab[i];
+				tab[i] = tab[i + 1];
+				tab[i + 1] = tmp;
+				i = -1;
+			}
+			i++;
+		}
+	}
+}
